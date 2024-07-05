72c34d5311809b461dd0510d86a923c1db16e36a
dforrester20549 <123339615+dforrester20549@users.noreply.github.com>
2024-07-04 18:59:28 -0600

    Codigo_Caso_Estudio
    Se crea el ejercicio del caso de estudio mas todo los componentes necesarios para su logica

---
+++ Caso_Estudio/Caso_Estudio/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Caso_Estudio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
